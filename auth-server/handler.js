c041c04fc895e971a84dab08ab47c3896c3e236e
xavsln <19362582+xavsln@users.noreply.github.com>
2022-09-18 14:37:50 +0200

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,16 +1,9 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-/**
- * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
- * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
- */
+
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-/**
- * Credentials are those values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
- */
 const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
@@ -29,21 +22,7 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-/**
- *
- * The first step in the OAuth process is to generate a URL so users can log in with
- * Google and be authorized to see your calendar. After logging in, they’ll receive a code
- * as a URL parameter.
- *
- */
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
-   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
-   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -60,23 +39,15 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-// The second step is where you need to use the code from the first step to generate an access token.
 module.exports.getAccessToken = async (event) => {
-  // The values used to instantiate the OAuthClient are at the top of the file
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-  // Decode authorization code extracted from the URL query
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
   return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-     */
-
     oAuth2Client.getToken(code, (err, token) => {
       if (err) {
         return reject(err);
@@ -85,23 +56,21 @@ module.exports.getAccessToken = async (event) => {
     });
   })
     .then((token) => {
-      // Respond with OAuth token
       return {
-        statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
         },
+        statusCode: 200,
         body: JSON.stringify(token),
       };
     })
     .catch((err) => {
-      // Handle error
       console.error(err);
       return {
-        statusCode: 500,
         headers: {
           "Access-Control-Allow-Origin": "*",
         },
+        statusCode: 500,
         body: JSON.stringify(err),
       };
     });
@@ -113,12 +82,11 @@ module.exports.getCalendarEvents = (event) => {
     client_secret,
     redirect_uris[0]
   );
-  // Decode authorization code extracted from the URL query
   const access_token = decodeURIComponent(
     `${event.pathParameters.access_token}`
   );
 
-  oAuth2Client.setCredendials({ access_token });
+  oAuth2Client.setCredentials({ access_token });
 
   return new Promise((resolve, reject) => {
     calendar.events.list(
@@ -139,8 +107,6 @@ module.exports.getCalendarEvents = (event) => {
     );
   })
     .then((results) => {
-      // Respond with event results
-      console.log("Results From the getCalendar", results);
       return {
         statusCode: 200,
         headers: {
@@ -150,7 +116,6 @@ module.exports.getCalendarEvents = (event) => {
       };
     })
     .catch((err) => {
-      // Handle error
       console.error(err);
       return {
         statusCode: 500,
